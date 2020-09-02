28edcc8d2a9f1e0015492ac39e3d60dc636080b6
Olav Fosse <fosseolav@gmail.com>
2020-09-02 12:12:42 +0200

    Add reset function to useField

--- anecdotes/src/hooks/useField.js
+++ anecdotes/src/hooks/useField.js
@@ -3,7 +3,8 @@ import { useState } from 'react'
 const useField = (type = 'text') => {
   const [value, setValue] = useState('')
   const onChange = (event) => setValue(event.target.value)
-  return { type, value, onChange}
+  const reset = () => setValue('')
+  return { type, value, onChange, reset}
 }
 
 export default useField
