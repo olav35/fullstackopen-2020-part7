f5f3ce0bd4dcc8636a393773e23ef88bbe57134d
Olav Fosse <fosseolav@gmail.com>
2020-09-02 12:08:40 +0200

    Make type argument of useField have default value 'text'

--- anecdotes/src/hooks/useField.js
+++ anecdotes/src/hooks/useField.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const useField = (type) => {
+const useField = (type = 'text') => {
   const [value, setValue] = useState('')
   const onChange = (event) => setValue(event.target.value)
   return { type, value, onChange}
